45e165cdcc5fa519a37554656f77184d886665b4
mateus <mateus.aluufc@gmail.com>
2016-09-07 22:15:12 -0300

    Add files via upload

---
+++ matrix_maker.py
@@ -0,0 +1,19 @@
+import random,os
+C = []
+lin = raw_input("QTD LINES>> ")
+col = raw_input ("QTD COLUMMS>> ")
+matrix = open("%sx%s.in" % (lin,col),'w')
+
+if  lin.isdigit() or col.isdigit():
+	lin = int(lin)
+	col = int(col)
+	for i in range(0,lin):
+	     C+= [[random.randint(1,100) for x in range(0,col)]]
+	    
+	C = str(C).replace("[","")
+	C = C.replace("]","\n")
+	C = C.replace(",","")
+	matrix.write(str(C))
+	matrix.close()
+else:
+	print "Invalid entry! Exiting..."
